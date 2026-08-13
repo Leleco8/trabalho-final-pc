6a8d8c8a1b06eb071fd02b0bf0595b70211f03a2
Gabriel Fernando <0404gabrielsantos@gmail.com>
2026-08-12 21:04:12 -0300

    lista de categorias e disponibilidade

---
+++ LISTA.py
@@ -0,0 +1,8 @@
+
+categoria = [
+    "Ficção Científica","Romance","Fantasia","Terror","Biografia","História","Autoajuda","Poesia","Suspense","Quadrinhos"
+]
+
+disp = [
+    "Disponível","Emprestado","Reservado"
+]
